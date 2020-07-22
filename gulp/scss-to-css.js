b6b6f9d06b24eebf5cf483169bc8f857c6579189
Zoe Holubkova <zzoehol@gmail.com>
2020-07-22 10:47:26 +0300

    Minify css

--- gulp/scss-to-css.js
+++ gulp/scss-to-css.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const cleanCss = require('gulp-clean-css');
 
 module.exports = function styles() {
     return gulp.src('src/styles/main.scss')
         .pipe(sass())
-        .pipe(gulp.dest('build/css'))
+        .pipe(cleanCss())
+        .pipe(gulp.dest('build/css'));
 }
